5ea1853f1aa90758f50ea6dbb1e3df7a9b721d6f
dudwns9416 <dudwns9416@naver.com>
2021-06-24 17:50:15 +0900

    #1 프로젝트 생성

---
+++ app/src/main/java/com/sc/imagesearch/ui/main/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.sc.imagesearch.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sc.imagesearch.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
